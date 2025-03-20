7b816887c4807c605eb0432b3bd2fc9d96bbacaa
sangmoki <yoochang4176@naver.com>
2025-03-20 22:07:10 +0900

    Feat: "기본 유닛들 Blend 구현 및 구매하지 않은 유닛들 잠금 구현"

--- Assets/Script/InGameSystem.cs
+++ Assets/Script/InGameSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,47 @@ using UnityEngine.UI;
 public class InGameSystem : MonoBehaviour
 {
     [SerializeField] Text CoinText;
+    [SerializeField] Button[] CharBtns;
+    [SerializeField] Image[] CoinImages;
+    [SerializeField] Image[] UnitImages;
+
+    int[] CharNums;
 
     // Start is called before the first frame update
     void Start()
     {
+        CharNums = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            CharNums[i] = PlayerPrefs.GetInt("Unit" + (i + 1));
+        }
+
         StartCoroutine(CoinIncrease());
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 캐릭터 보유 여부 확인 (상점에서 구매했는지)
+        for (int i = 0; i < 5; i++)
+        {
+            if (CharNums[i] == 1)
+            {
+                // 있다면 버튼 활성화
+                CharBtns[i].interactable = true;
+                UnitImages[i].color = Color.white;
+                CoinImages[i].color = Color.white;
+            }
+            else if (CharNums[i] == 0)
+            {
+                // 없다면 버튼 비활성화
+                CharBtns[i].interactable = false;
+                UnitImages[i].color = Color.gray;
+                CoinImages[i].color = Color.gray;
+            }
+        }
+
         CoinText.text = UnitSpawn.CoinAmount.ToString();
     }
 
